254c072622d59b663726f1aba5a460453c05fcb0
ajha <ajha@cloudsmith.io>
2025-03-12 21:11:04 +0530

    changes in master-workflow-53

--- nyapkg/setup.py
+++ nyapkg/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nyapkg",
-    version="2.0.1",
+    version="4.0.1",
     author="aayush-jha",
     description="abhi bnaya h using GHA",
     packages=find_packages(),
